c9569dcb708e5904cfc05cc31474f9e7c3e13794
Teemu Kokkonen <teemu@rautasydan.org>
2014-09-15 11:36:09 +0300

    Added PlaySoundOnCollision

---
+++ Utilities/PlaySoundOnCollision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundOnCollision : MonoBehaviour {
+	public AudioClip sound;
+
+	void Start(){
+		if (!audio) {
+			gameObject.AddComponent<AudioSource>();
+		}
+	}
+	void OnCollisionEnter(Collision c){
+		PlaySound ();
+	}
+	void OnCollisionEnter2D(Collision2D c){
+		PlaySound ();
+	}
+
+	void PlaySound(){
+		audio.PlayOneShot (sound);
+	}
+}
