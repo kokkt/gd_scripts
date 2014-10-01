9888ebee7285c2d77138546c22f0867cafa339b8
Teemu Kokkonen <teemu.kokkonen@ksao.fi>
2014-10-01 14:52:48 +0300

    Improved MotionWave Hopefully now ignored .meta files

---
+++ Motion/MotionWave.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotionWave : MotionPattern{
+	public enum Method {Sin, Cos, Tan}
+	public Method method = Method.Sin;
+	public Vector3 direction = new Vector3 ();
+	public float waveFrequency = 1;
+	float easing = 0.1f;
+	float frame = 0;
+	float waveFactor = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		switch (method) {
+				case Method.Cos:
+						waveFactor = Mathf.Cos (frame);
+						break;
+				case Method.Sin:
+						waveFactor = Mathf.Sin (frame);
+						break;
+				case Method.Tan:
+						waveFactor = Mathf.Tan (frame);
+						break;
+				}
+		transform.Translate (direction*waveFactor*easing);
+		frame += waveFrequency*Time.deltaTime;
+	}
+}
---
+++ Utilities/DestroyWhenChildless.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyWhenChildless : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (transform.childCount <= 0) {
+			Destroy(gameObject);
+				}
+	}
+}
